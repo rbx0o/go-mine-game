8b5eb8b87a3e2457b1b0e053f766bfd2b1c212f3
rbx <mail@error.com>
2026-06-07 09:20:29 +0300

    Завершил работу с Miner

--- domain/equipment.go
+++ domain/equipment.go
@@ -1,3 +1,73 @@
 package domain
 
 // Файл в котором будет описано оборудования для предприятия и базовая логика
+
+/*==================================================
+
+Оборудование покупается не для повышения производительности,
+а для того, чтобы пройти игру.
+У нас есть 3 оборудования, которые необходимо купить:
+- Кирки - 3000 угля
+- Вентиляция - 15000 угля
+- Вагонетки - 50000
+Лучше сделать интерфейс и уже на его основе 3 структуры.
+
+==================================================*/
+
+type Equipment interface {
+	Buy()
+}
+
+//==================================================
+
+type Pickaxe struct {
+	cost     Coal // Стоимость - 3000
+	isBought bool // Куплено/не куплено
+}
+
+func InitPickaxe() *Pickaxe {
+	return &Pickaxe{
+		cost:     3000,
+		isBought: false,
+	}
+}
+
+func (p *Pickaxe) Buy() {
+	p.isBought = true
+}
+
+//==================================================
+
+type Ventilation struct {
+	cost     Coal // Стоимость - 15000
+	isBought bool // Куплено/не куплено
+}
+
+func InitVentilation() *Ventilation {
+	return &Ventilation{
+		cost:     15000,
+		isBought: false,
+	}
+}
+
+func (v *Ventilation) Buy() {
+	v.isBought = true
+}
+
+//==================================================
+
+type Trolleys struct {
+	cost     Coal // Стоимость - 50000
+	isBought bool // Куплено/не куплено
+}
+
+func InitTrolleys() *Trolleys {
+	return &Trolleys{
+		cost:     50000,
+		isBought: false,
+	}
+}
+
+func (t *Trolleys) Buy() {
+	t.isBought = true
+}
--- domain/miner.go
+++ domain/miner.go
@@ -1,5 +1,11 @@
 package domain
 
+import (
+	"context"
+	"sync"
+	"time"
+)
+
 // Файл в котором будут описаны шахтеры и базовая логика
 
 /*==================================================
@@ -7,12 +13,211 @@ package domain
 Есть 3 типа шахтёров. Для них лучше создать интерфейс.
 Шахтёры должны уметь начинать и завершать работу, а также
 возвращать информацию о себе.
+Шахтёры - структуры, которые хранят в себе информацию о том,
+как они должны работать.
+Нужно также уметь возвращать некий snapshot, состояния шахтёра
+в данный момент. Для этого можно создать структуры MinerInfo,
+которая будет хранить информацию о шахтёре в данный момент и
+возвращать её в методе GetInfo(). По сути во вне нужно возвращать
+только сколько энергии осталось у конкретного шахтёра (в теории
+можно ещё ID и класс шахтёра)
 
-Run() - запускает
+Также у каждого класса шахтёра будет поле Mutex. Почему так?
+Mutex контролирует данные внутри одного шахтёра.
+Один Mutex для всех шахтёров не нужен, потому что у каждого
+разные данные.
+Mutex нужен лишь в рамках одного конкретного шахтёра, потому что
+гонка данных может возникнуть, если работа с данными конкретного
+экземпляра шахтёра будет производиться в нескольких функциях
+одновременно.
+
+Run() - запускает горутину с шахтёром
+GetInfo() - возвращает информацию об одном конкретном шахтёре
+в данный момент
 
 ==================================================*/
 
 type Miner interface {
-	Run()
-	GetInfo()
+	/*
+		Run() запускает работу шахтёра.
+		Запускается добыча угля в горутине по заданным параметрам (энергия, добыча и т.п.).
+		В аргументах принимается контекст работы данной горутины (от предприятия).
+		Возвращается канал, в которой передаётся добытый уголь.
+	*/
+	Run(context.Context) <-chan Coal
+
+	/*
+		GetInfo() возвращает информацию о шахтёре.
+		По сути просто возвращает копию поля info шахтёра
+	*/
+	GetInfo() MinerInfo
+}
+
+//==================================================
+
+type MinerInfo struct {
+	energyLeft int // Сколько энергии осталось
+}
+
+func InitMinerInfo(energy int) MinerInfo {
+	return MinerInfo{
+		energyLeft: energy,
+	}
+}
+
+//==================================================
+
+type SmallMiner struct {
+	salary    Coal          // Оплата труда - 5
+	energy    int           // Энергия - 30
+	coalCount Coal          // За одну добычу - 1
+	timeout   time.Duration // Задержка между добычами - 3
+	info      MinerInfo     // Информация о шахтёре в данный момент
+	mtx       sync.Mutex    // Mutex для контроля гонки данных
+}
+
+func InitSmallMiner() *SmallMiner {
+	return &SmallMiner{
+		salary:    5,
+		energy:    30,
+		coalCount: 1,
+		timeout:   3 * time.Second,
+		info:      InitMinerInfo(30),
+	}
+}
+
+func (m *SmallMiner) GetInfo() MinerInfo {
+	m.mtx.Lock()
+	defer m.mtx.Unlock()
+
+	return m.info
+}
+
+func (m *SmallMiner) Run(ctx context.Context) <-chan Coal {
+	ch := make(chan Coal)
+
+	go func() {
+		defer close(ch)
+
+		for i := 0; i < m.energy; i++ {
+			select {
+			case <-ctx.Done():
+				return
+			case <-time.After(m.timeout):
+				ch <- m.coalCount
+
+				m.mtx.Lock()
+				m.info.energyLeft--
+				m.mtx.Unlock()
+			}
+		}
+	}()
+
+	return ch
+}
+
+//==================================================
+
+type NormalMiner struct {
+	salary    Coal          // Оплата труда - 50
+	energy    int           // Энергия - 45
+	coalCount Coal          // За одну добычу - 3
+	timeout   time.Duration // Задержка между добычами - 2
+	info      MinerInfo     // Информация о шахтёре в данный момент
+	mtx       sync.Mutex    // Mutex для контроля гонки данных
+}
+
+func InitNormalMiner() *NormalMiner {
+	return &NormalMiner{
+		salary:    50,
+		energy:    45,
+		coalCount: 3,
+		timeout:   2 * time.Second,
+		info:      InitMinerInfo(45),
+	}
+}
+
+func (m *NormalMiner) GetInfo() MinerInfo {
+	m.mtx.Lock()
+	defer m.mtx.Unlock()
+
+	return m.info
+}
+
+func (m *NormalMiner) Run(ctx context.Context) <-chan Coal {
+	ch := make(chan Coal)
+
+	go func() {
+		defer close(ch)
+
+		for i := 0; i < m.energy; i++ {
+			select {
+			case <-ctx.Done():
+				return
+			case <-time.After(m.timeout):
+				ch <- m.coalCount
+
+				m.mtx.Lock()
+				m.info.energyLeft--
+				m.mtx.Unlock()
+			}
+		}
+	}()
+
+	return ch
+}
+
+//==================================================
+
+type StrongMiner struct {
+	salary    Coal          // Оплата труда - 450
+	energy    int           // Энергия - 60
+	coalCount Coal          // За одну добычу - 10
+	timeout   time.Duration // Задержка между добычами - 1
+	info      MinerInfo     // Информация о шахтёре в данный момент
+	progress  Coal          // coalCount увеличивается на progress - 3
+	mtx       sync.Mutex    // Mutex для контроля гонки данных
+}
+
+func InitStrongMiner() *StrongMiner {
+	return &StrongMiner{
+		salary:    450,
+		energy:    60,
+		coalCount: 10,
+		timeout:   1 * time.Second,
+		info:      InitMinerInfo(60),
+		progress:  3,
+	}
+}
+
+func (m *StrongMiner) GetInfo() MinerInfo {
+	m.mtx.Lock()
+	defer m.mtx.Unlock()
+
+	return m.info
+}
+
+func (m *StrongMiner) Run(ctx context.Context) <-chan Coal {
+	ch := make(chan Coal)
+
+	go func() {
+		defer close(ch)
+
+		for i := 0; i < m.energy; i++ {
+			select {
+			case <-ctx.Done():
+				return
+
+			case <-time.After(m.timeout):
+				ch <- m.coalCount
+
+				m.mtx.Lock()
+				m.info.energyLeft--
+				m.coalCount += m.progress
+				m.mtx.Unlock()
+			}
+		}
+	}()
+
+	return ch
 }
