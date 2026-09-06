68c4b8a3d1977ecc64c161181736b1ce1ee9cc43
rbx0o <77440436+rbx0o@users.noreply.github.com>
2026-09-06 20:45:41 +0300

    feat: add GetEnterpriseInfo() func refs #17

--- service/enterprise.go
+++ service/enterprise.go
@@ -1,5 +1,7 @@
 package service
 
+import "github.com/rbx0o/go-mine-game/domain"
+
 /*==================================================
 
 Для удобства один пакет разбит на несколько файлов.
@@ -7,3 +9,27 @@ package service
 Здесь описаны функции, связанные с работой предприятия.
 
 ==================================================*/
+
+type EnterpriseInfo struct {
+	Balance domain.Coal
+
+	ActiveMiners   map[domain.ID]domain.Miner
+	InactiveMiners map[domain.ID]domain.Miner
+
+	AllEquipment map[domain.EquipmentType]bool
+}
+
+/*
+GetEnterpriseInfo
+возвращает структуру данных с информацией о предприятии в данный момент
+*/
+func (g *GameService) GetEnterpriseInfo() EnterpriseInfo {
+	return EnterpriseInfo{
+		Balance: g.enterprise.Balance,
+
+		ActiveMiners:   g.GetActiveMiners(),
+		InactiveMiners: g.GetInactiveMiners(),
+
+		AllEquipment: g.GetEquipmentInfo(),
+	}
+}
