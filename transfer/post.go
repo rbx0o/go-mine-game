996bdc59e6344df113dbed7dcd6febcd0122332e
rbx0o <77440436+rbx0o@users.noreply.github.com>
2026-09-15 21:19:53 +0300

    feat: add StartGame handler refs #27

--- transfer/post.go
+++ transfer/post.go
@@ -1,3 +1,22 @@
 package transfer
 
+import "net/http"
+
 // Здесь будут описаны обработчики с методом POST
+
+/*
+pattern:	/game/start
+method:		POST
+info:
+
+succeed:
+  - status code:	201 Created
+  - response body:	JSON created task
+
+failed:
+  - status code:	400 BadRequest, 409 Conflict, 500 InternalServerError
+  - response body: 	JSON with error + time
+*/
+func (h *HTTPHandlers) StartGame(response http.ResponseWriter, request *http.Request) {
+	h.gameService.Start()
+}
