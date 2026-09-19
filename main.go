9a21d719fa58c941c38c5c47b8e68e5e1d26343b
rbx0o <77440436+rbx0o@users.noreply.github.com>
2026-09-19 19:08:06 +0300

    feat: add .env, HTTP server, start app refs #27, #25, #26, #30

--- main.go
+++ main.go
@@ -1,5 +1,40 @@
 package main
 
-func main() {
+import (
+	"fmt"
+	"os"
 
+	"github.com/joho/godotenv"
+	"github.com/rbx0o/go-mine-game/service"
+	"github.com/rbx0o/go-mine-game/transfer"
+)
+
+func main() {
+	_ = godotenv.Load()
+
+	game := service.InitGameService()
+	fmt.Println("Game service initialized")
+
+	httpHandlers := transfer.NewHTTPHandlers(game)
+	fmt.Println("HTTP handlers initialized")
+
+	server := transfer.NewHTTPServer(httpHandlers)
+	fmt.Println("HTTP server initialized")
+
+	hostname, ok := os.LookupEnv("HTTP_HOSTNAME")
+	if !ok {
+		fmt.Println("HTTP_HOSTNAME is required")
+		return
+	}
+	port, ok := os.LookupEnv("HTTP_PORT")
+	if !ok {
+		fmt.Println("HTTP_PORT is required")
+		return
+	}
+
+	fmt.Printf("Start HTTP server on %v:%v\n", hostname, port)
+	if err := server.StartServer(hostname, port); err != nil {
+		fmt.Printf("HTTP server error %v\n", err)
+	}
+	fmt.Println("Game Over")
 }
--- transfer/server.go
+++ transfer/server.go
@@ -1,3 +1,40 @@
 package transfer
 
+import (
+	"errors"
+	"fmt"
+	"net/http"
+)
+
 // Здесь будет описана логика работы HTTP сервера
+
+type HTTPServer struct {
+	httpHandlers *HTTPHandlers
+}
+
+/*
+NewHTTPServer конструктор для создания объекта типа HTTPServer
+*/
+func NewHTTPServer(httpHandlers *HTTPHandlers) *HTTPServer {
+	return &HTTPServer{
+		httpHandlers: httpHandlers,
+	}
+}
+
+/*
+StartServer запускает HTTP сервер
+*/
+func (h *HTTPServer) StartServer(hostname string, port string) error {
+	mux := http.NewServeMux()
+
+	mux.HandleFunc("POST /game/start", h.httpHandlers.StartGame)
+
+	addr := fmt.Sprintf("%v:%v", hostname, port)
+	err := http.ListenAndServe(addr, mux)
+
+	if err != nil && !errors.Is(err, http.ErrServerClosed) {
+		return err
+	}
+
+	return nil
+}
