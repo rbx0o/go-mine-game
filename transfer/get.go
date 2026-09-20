372669be8d25e7abf1f1189b166b1a9f22add37c
rbx0o <77440436+rbx0o@users.noreply.github.com>
2026-09-20 16:55:10 +0300

    feat: add GetState handler refs #35

--- transfer/get.go
+++ transfer/get.go
@@ -1,3 +1,34 @@
 package transfer
 
+import (
+	"net/http"
+	"time"
+
+	"github.com/rbx0o/go-mine-game/service"
+)
+
 // Здесь будут описаны обработчики с методом GET
+
+/*
+pattern:	/game/state
+method:		GET
+info:		-
+
+succeed:
+  - status code:	200 OK
+  - response body:	JSON game status + time
+
+failed:
+  - status code:	500 InternalServerError
+  - response body: 	JSON with error + time
+*/
+
+func (h *HTTPHandlers) GetState(response http.ResponseWriter, request *http.Request) {
+	state := h.gameService.GetState()
+	dto := SuccessResponseDTO[service.GameState]{
+		Data:    &state,
+		Message: "",
+		Time:    time.Now(),
+	}
+	SendJSON(response, dto, http.StatusOK)
+}
--- transfer/server.go
+++ transfer/server.go
@@ -28,6 +28,7 @@ func (h *HTTPServer) StartServer(hostname string, port string) error {
 	mux := http.NewServeMux()
 
 	mux.HandleFunc("POST /game/start", h.httpHandlers.StartGame)
+	mux.HandleFunc("GET /game/state", h.httpHandlers.GetState)
 
 	addr := fmt.Sprintf("%v:%v", hostname, port)
 	err := http.ListenAndServe(addr, mux)
